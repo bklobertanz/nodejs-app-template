a2e10e895fee78fba810f9b633184800c917ea8d
Barís Klobertanz <klobertanz@gmail.com>
2022-05-11 12:59:00 -0400

    feat(App): first template version

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/* delete me */
+import 'dotenv/config'
+
+console.log('hola mundo')
+
+function sum(a: number, b: number): number {
+  return a + b
+}
+console.log(sum(1, 1))
+console.log(process.env.test)
